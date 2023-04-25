50fd4ce5edb695433d1ecbfad044a70d9460d5c3
Feodor Kachinsky <fedor15zil@gmail.com>
2023-04-25 18:34:39 +0300

    added endpoint routings

---
+++ src/main/scala/router/CovidRouter.scala
@@ -0,0 +1,40 @@
+package com.innowise
+package router
+
+import cats.effect.{Concurrent, Sync}
+import cats.implicits.*
+import com.innowise.entity.GeneralCaseData
+import com.innowise.service.CovidService
+import io.circe.{Decoder, Encoder}
+import org.http4s.*
+import org.http4s.circe.*
+import org.http4s.dsl.Http4sDsl
+import org.http4s.dsl.impl.QueryParamDecoderMatcher
+
+object CovidRouter:
+
+  private object FromDateQueryParamMatcher extends QueryParamDecoderMatcher[String]("from")
+
+  private object ToDateQueryParamMatcher extends QueryParamDecoderMatcher[String]("to")
+  
+  def getAllCountriesRoute[F[_] : Sync](covidApiService: CovidService[F]): HttpRoutes[F] =
+    val dsl = new Http4sDsl[F] {}
+    import dsl.*
+    HttpRoutes.of[F] {
+      case GET -> Root / "covid" / "countries" =>
+        covidApiService.getAllCountries.flatMap { countryList =>
+          Ok(countryList)
+        }
+    }
+    
+  def getMinMaxCasesForCountryRoute[F[_] : Sync](covidApiService: CovidService[F]): HttpRoutes[F] =
+    val dsl = new Http4sDsl[F] {}
+    import dsl.*
+    HttpRoutes.of[F] {
+      case GET -> Root / "covid" / "countries" / country :?
+        FromDateQueryParamMatcher(from) +& ToDateQueryParamMatcher(to) => {
+          covidApiService.getMinMaxCovidCaseDataForPeriod(country, from, to).flatMap { minMaxCovidCaseList =>
+            Ok(minMaxCovidCaseList)
+          }
+        }
+    }
